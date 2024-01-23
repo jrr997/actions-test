69910c28a3929a6335ce157f93cd555fddbe4ae5
jrr997 <zackchangjx@foxmail.com>
2024-01-23 14:57:46 +0800

    test

--- dist/index.js
+++ dist/index.js
@@ -29045,7 +29045,7 @@ var DocsLang;
 ;
 const excludeDirs = ['__tests__', '_util', 'back-top', 'col', 'locale', 'row', 'style', 'theme', 'version'];
 const ANTD_GITHUB = {
-    OWNER: 'ant-design',
+    OWNER: 'jrr997',
     REPO: 'ant-design',
     EN_DOC_NAME: 'index.en-US.md',
     ZH_DOC_NAME: 'index.zh-CN.md',
--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ enum DocsLang {
 const excludeDirs = ['__tests__', '_util', 'back-top', 'col', 'locale', 'row', 'style', 'theme', 'version'];
 
 const ANTD_GITHUB = {
-  OWNER: 'ant-design',
+  OWNER: 'jrr997',
   REPO: 'ant-design',
   EN_DOC_NAME: 'index.en-US.md',
   ZH_DOC_NAME: 'index.zh-CN.md',
